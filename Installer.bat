e33a968d286e6c3b4858743128b2b0bc80e0ac05
BanRioT <111208999+BanRioT@users.noreply.github.com>
2024-08-20 17:25:10 -0700

    Add files via upload

---
+++ Installer.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Installing required Python packages...
+pip install requests
+pip install scapy
+pip install dnspython
+pip install whois
+pip install beautifulsoup4
+pip install instaloader
+pip install statistics
+echo All packages installed successfully.
+pause
