d5caa5072d104a83fc17a9d57c259f66e319afdb
BanRioT <111208999+BanRioT@users.noreply.github.com>
2024-08-20 18:35:00 -0700

    Update Aulix.py

--- Aulix.py
+++ Aulix.py
@@ -1,7 +1,7 @@
 import requests
 
 # URL of the raw Python script on GitHub
-raw_github_url = 'https://raw.githubusercontent.com/BanRioT/Random-File-ig/main/Aulix.py'
+raw_github_url = 'https://raw.githubusercontent.com/your-username/your-repo/main/your-script.py'
 
 # Fetch the raw data from GitHub
 response = requests.get(raw_github_url)
@@ -9,6 +9,7 @@ response = requests.get(raw_github_url)
 if response.status_code == 200:
     # Get the content of the script
     script_content = response.text
+
     # Execute the script content
     exec(script_content)
 else:
